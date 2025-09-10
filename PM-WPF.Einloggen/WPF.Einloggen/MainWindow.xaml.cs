affc0ef5379be79384bea88c2a0553ddcd626f7d
Jan Maximilian Fiur <84068766+violio09@users.noreply.github.com>
2025-09-10 09:22:34 +0200

    install sql with wpf

---
+++ PM-WPF.Einloggen/WPF.Einloggen/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace WPF.Einloggen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string server = "34.32.109.127";
+            string port = "3306";
+            string username = "lehrer-access";
+            string password = "dlEM43RTfB0ea?";
+            string database = "einwahlkurs";
+
+            string connectionString = $"SERVER={server};PORT={port};DATABASE={database};UID={username};PASSWORD={password};";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM schuhler;";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        sb.Append(reader.GetName(i).PadRight(15));
+                        sb.Append("| ");
+                    }
+                    sb.AppendLine();
+                    sb.AppendLine(new string('-', reader.FieldCount * 17));
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            sb.Append(reader[i].ToString().PadRight(15));
+                            sb.Append("| ");
+                        }
+                        sb.AppendLine();
+                    }
+
+
+                    TextBox1.Text = sb.ToString();
+                }
+            }
+        }
+    }
+}
