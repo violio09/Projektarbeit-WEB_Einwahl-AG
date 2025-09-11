df3fd6da9e120f24348e2bafaa3499e57fa5355a
ikillgato <maxcarstens@gmx.de>
2025-09-11 09:04:54 +0200

    update text boxe

--- PM.WpfApp1UmfrageBlatt4.8/WpfApp1UmfrageBlatt4.8/MainWindow.xaml.cs
+++ PM.WpfApp1UmfrageBlatt4.8/WpfApp1UmfrageBlatt4.8/MainWindow.xaml.cs
@@ -45,11 +45,6 @@ namespace UmfrageBlatt
             zweitwahl = Convert.ToInt32(cmbZweitwahl.SelectedItem);
             drittwahl = Convert.ToInt32(cmbDrittwahl.SelectedItem);
 
-
-            // Auswahl als String speichern
-            string erstwahlAuswahl = cmbErstwahl.SelectedItem as string;
-            string zweitwahlAuswahl = cmbZweitwahl.SelectedItem as string;
-            string drittwahlAuswahl = cmbDrittwahl.SelectedItem as string;
             // Beispielhafte Ausgabe im Textfeld
 
             string ausgabe =
@@ -143,8 +138,9 @@ namespace UmfrageBlatt
 
                 string query = $"INSERT INTO `schuhler` (`s_ID`, `s_Name`, `s_Vorname`, `s_Klasse`, `s_erstWahl`, `s_zweitWahl`, `s_drittWahl`, `s_Username`, `s_Password`) VALUES ({schuhlerId}, '{nachname}', '{vorname}',{klassenstufe},{erstwahl},{zweitwahl},{drittwahl}, '{benutzer}', '{passwort}');";
                 MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
             }
-            MessageBox.Show("Daten wurden in die Datenbank eingetragen.");
         }
 
     }
